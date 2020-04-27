d072287df27ad0bbee3fc9368372a17b95af91a0
TianhangZhang <tz63@cornell.edu>
2020-04-27 15:43:59 -0400

    Add HW1

---
+++ Calculate.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+public class Calculate{
+    public static void main(String[] args){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<i;j++){
+                 System.out.print(" ");
+            }
+            for(int k=0;k<5-i; k++){
+                 System.out.print("* ");
+            }
+        System.out.println();
+        }
+    }
+}
